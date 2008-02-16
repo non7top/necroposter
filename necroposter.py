2770b3a5785b3c92f009510956251e94f1b3b16f
Vladimir V. Berezhnoy <non7top@gmail.com>
2008-02-16 14:57:29 +0000

    ***

--- necroposter.py
+++ necroposter.py
@@ -12,9 +12,47 @@ from lxml import etree
 
 class necroposter():
 	def __init__(self):
+		self.parser = etree.HTMLParser()
+
+	def dw_wapage(self,  wa_addr):
+		print wa_addr
+		req = urllib2.Request(wa_addr)
+		
+		try:
+			handle = urllib2.urlopen(req)
+		except IOError:
+			print "ioerror"
+		self.thepage = unicode( handle.read(),    "cp1251")
+		self.tree = etree.parse(StringIO(self.thepage), self.parser)
+
+	def get_title(self):
+		r = self.tree.xpath('/html/body/center[2]/table[6]/tr/td/table/tr/td[5]/table/tr/td[3]/font/b')
+		title = r[0].text[:-2].strip()
+		return title
+
+	def get_year(self):
+		r = self.tree.xpath("/html/body/center[2]/table[6]/tr/td/table/tr/td[5]/table/tr/td[3]/a/font")
+		#print etree.tostring(r[0])
+		year = r[0].text.strip()
+		return year
+		
+	def get_names(self):
+		self.names=[]
+		r = self.tree.xpath("/html/body/center[2]/table[6]/tr/td/table/tr/td[5]/table/tr/td[3]/br")
+		for q in r:
+			self.names.append(q.tail)
+		print self.names
+	
+	
+	def cat(self, xpath):
+		r = self.tree.xpath(xpath)
+		print etree.tostring(r[0])
+	def lsch(self, xpath):
+		r = self.tree.xpath(xpath)
+		for c in r[0]:
+			print c.tag
+		 
 		pass
-
-
 	def addlink(self, url):
 	
 		theurl = 'http://litops.hq.aaanet.ru/cts_support/cts_s_jinn'
@@ -130,5 +168,11 @@ class necroposter():
 			#print len(r)
 	
 
-#qqq=jinn()
-#qqq.check()
+qqq=necroposter()
+qqq.dw_wapage("http://www.world-art.ru/animation/animation.php?id=2699")
+#print qqq.get_title()
+#print qqq.get_year()
+print qqq.get_names()
+#qqq.cat(p)
+#qqq.lsch(p)
+print "finish"
