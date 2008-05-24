0b7567a27cd87def1ab9fe530fcfbd363a77ca6e
Vladimir V. Berezhnoy <non7top@gmail.com>
2008-05-24 14:35:39 +0000

    svn ignore && test

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/utsr/bin/env python
+# -*- coding: utf8 -*-
+
+import sys
+import base64
+import re
+import urllib
+import urllib2
+from StringIO import StringIO
+
+from lxml import etree
+from necroposter import necroposter
+
+np=necroposter()
+np.dw_wapage("3284")
+np.init_data()
+#np.get_desc()
+np.gen_bbcode()
+
