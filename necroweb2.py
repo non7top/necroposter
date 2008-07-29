9dd5dc70dfcee2e91e3699f15903f028ccfc8ab0
Vladimir V. Berezhnoy <non7top@gmail.com>
2008-07-29 12:30:01 +0000

    add fcgi script

---
+++ necroweb2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import os, sys
+from paste.request import parse_formvars
+from Cheetah.Template import Template
+from paste.debug import prints
+
+cp=os.path.dirname(__file__)
+sys.path.append(cp)
+from necroposter import necroposter
+
+from fcgi import WSGIServer
+
+def application(environ, start_response):
+        fields = parse_formvars(environ)
+        tplvars={"tpl":""}
+        if environ['REQUEST_METHOD'] == 'POST' and fields['wa_addr'] != "":
+                np=necroposter()
+                np.dw_wapage(fields['wa_addr'])
+                np.init_data()
+                tplvars['tpl']=str(np.gen_bbcode().encode('utf-8'))
+        else:
+        	pass
+        	
+        start_response('200 OK', [('content-type', 'text/html; charset=utf-8')])
+	
+        tmpl_fl=os.path.join(cp, 'templates/form.tmpl')
+        k=Template(file=tmpl_fl,searchList=[tplvars])
+        return [str(k)]
+
+        
+WSGIServer(application, bindAddress = '/tmp/fcgi.sock').run()
