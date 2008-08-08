54ad7fdb3bf9faca9ecc47a30a8f5ced18acf650
Vladimir V. Berezhnoy <non7top@gmail.com>
2008-08-08 12:48:07 +0000

    some fixes web

--- necroweb2.py
+++ necroweb2.py
@@ -15,14 +15,18 @@ from fcgi import WSGIServer
 def application(environ, start_response):
         fields = parse_formvars(environ)
         tplvars={"tpl":""}
+        print fields
         if environ['REQUEST_METHOD'] == 'POST' and fields['wa_addr'] != "":
                 np=necroposter()
                 np.dw_wapage(fields['wa_addr'])
                 np.init_data()
+	        #print fields['doupload']
                 tplvars['tpl']=str(np.gen_bbcode().encode('utf-8'))
+                
+		tplvars["page_id"]=np.pagenum       	
         else:
         	pass
-        	
+	
         start_response('200 OK', [('content-type', 'text/html; charset=utf-8')])
 	
         tmpl_fl=os.path.join(cp, 'templates/form.tmpl')
