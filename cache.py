aa78e57a2030949aed3818b824fafdda62967cba
Vladimir V. Berezhnoy <non7top@gmail.com>
2009-03-05 10:54:14 +0300

    Fix indent

--- cache.py
+++ cache.py
@@ -7,43 +7,43 @@ import logging
 
 
 class cache():
-        def __init__ (self, storage, enable = 1):
-                self.storage=storage
+    def __init__ (self, storage, enable = 1):
+        self.storage=storage
 
-        def get(self, fname):
-                k = os.path.join(self.storage, fname)
-                l = open(k)
-                m = l.read()
-                l.close()
-                return m
+    def get(self, fname):
+        k = os.path.join(self.storage, fname)
+        l = open(k)
+        m = l.read()
+        l.close()
+        return m
 
-        def put(self, fname, content):
-                k = os.path.join(self.storage, fname)
-                l = open(k, 'w')
-                n = l.write(content)
-                l.close()
-                return n
+    def put(self, fname, content):
+        k = os.path.join(self.storage, fname)
+        l = open(k, 'w')
+        n = l.write(content)
+        l.close()
+        return n
 
-        def incache(self, fname):
-                k = os.path.join(self.storage, fname)
-                if os.path.isfile(k):
-                        return 1
-                else:
-                        return 0
+    def incache(self, fname):
+        k = os.path.join(self.storage, fname)
+        if os.path.isfile(k):
+            return 1
+        else:
+            return 0
 
-	def get_url(self, url, fname):
-		if self.incache(fname):
-                        logging.debug ("Not re-downloading file")
-                        page_body=self.get(fname)
-                else:
-                        #logging.info( "WA page is %s" % self.wa_addr )
-                        req = urllib2.Request(url)
+    def get_url(self, url, fname):
+        if self.incache(fname):
+            logging.debug ("Not re-downloading file")
+            page_body=self.get(fname)
+        else:
+            #logging.info( "WA page is %s" % self.wa_addr )
+            req = urllib2.Request(url)
 
-                        try:
-                                handle = urllib2.urlopen(req)
-                        except IOError:
-                                print "ioerror"
-                        page_body=handle.read()
-                        handle.close()
-                        self.put(fname,page_body)
-                return page_body
+            try:
+                handle = urllib2.urlopen(req)
+            except IOError:
+                print "ioerror"
+                page_body=handle.read()
+                handle.close()
+                self.put(fname,page_body)
+        return page_body
