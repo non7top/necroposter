f727d0e60319ccf5fcd91f86e5d9df0575a43aee
Vladimir V. Berezhnoy <non7top@gmail.com>
2009-03-09 11:56:21 +0300

    Fix indent

--- cast.py
+++ cast.py
@@ -16,36 +16,34 @@ from cache import cache
 import os
 
 class cast():
-	def __init__(self, wa_id, cache):
-                parser = etree.HTMLParser()
-		self.id=wa_id
-                self.cache = cache
-		wa_addr = "http://world-art.ru/animation/animation_full_cast.php?id=%s" % self.id
-                fname='cast.cache'
-                body = self.cache.dw_html(wa_addr, fname)
+    def __init__(self, wa_id, cache):
+        parser = etree.HTMLParser()
+        self.id=wa_id
+        self.cache = cache
+        wa_addr = "http://world-art.ru/animation/animation_full_cast.php?id=%s" % self.id
+        fname='cast.cache'
+        body = self.cache.dw_html(wa_addr, fname)
 
-                self.thepage = unicode(body, "cp1251")
-                self.tree = etree.parse(StringIO(self.thepage), parser)
-                self.get_actors()
+        self.thepage = unicode(body, "cp1251")
+        self.tree = etree.parse(StringIO(self.thepage), parser)
+        self.get_actors()
 
-	def get_actors(self):
-		self.actors=[]
-		p = "//b[.  = '" + u'Роли озвучивали:' + "']"
-		r = self.tree.xpath(p)[0].getparent().getparent().getparent().getparent()
-                '''walk through all tr elements'''
-		for q in r:
-			#self.actors.append(q.text)
-                        try:
-                                rr = q.xpath("td[2]/a")
-                                ac_name = rr[0].text
-                                ac_id = utils.getnum(rr[0].get("href"))
-                                ac_link = "http://world-art.ru/people.php?id=%s" % ac_id
-                                rr = q.xpath("td[3]")
-                                ac_role = rr[0].text[2:]
-                                self.actors.append({'ac_name':ac_name,'ac_id':ac_id,'ac_role':ac_role,
-                                    'ac_link':ac_link})
-                        except IOError:
-                                pass
-                        except IndexError:
-                                pass
-                return self.actors
+    def get_actors(self):
+        self.actors=[]
+        p = "//b[.  = '" + u'Роли озвучивали:' + "']"
+        r = self.tree.xpath(p)[0].getparent().getparent().getparent().getparent()
+        '''walk through all tr elements'''
+        for q in r:
+            try:
+                rr = q.xpath("td[2]/a")
+                ac_name = rr[0].text
+                ac_id = utils.getnum(rr[0].get("href"))
+                ac_link = "http://world-art.ru/people.php?id=%s" % ac_id
+                rr = q.xpath("td[3]")
+                ac_role = rr[0].text[2:]
+                self.actors.append({'ac_name':ac_name,'ac_id':ac_id,'ac_role':ac_role, 'ac_link':ac_link})
+            except IOError:
+                pass
+            except IndexError:
+                pass
+        return self.actors
